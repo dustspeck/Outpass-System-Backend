3b7c8af5f4ac210fbcfdf3bff3dd5b7fc9fa72bd
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-03-06 19:54:09 +0530

    add: rsa encrypt decrypt

---
+++ src/handlers/encryption.handler.ts
@@ -0,0 +1,40 @@
+import NodeRSA from 'node-rsa';
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+
+let rsa: NodeRSA | null = null;
+
+try {
+	rsa = new NodeRSA(PRIVATE_KEY);
+} catch (error) {
+	console.log(error);
+}
+
+export const encryptData = (data: string) => {
+	if (!rsa) return null;
+	try {
+		const enc_data = rsa.encrypt(`${data}`, 'base64');
+		return enc_data;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
+export const decryptData = (data: string) => {
+	if (!rsa) return null;
+	try {
+		const dec_data = rsa.decrypt(`${data}`, 'utf8');
+		return dec_data;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
+// -----BEGIN PUBLIC KEY-----
+// MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjpCTYNEdHhcXN1pv1L+yw7u1y
+// u0AQQXqe0mE7bdT7TRyiM5o5lr63DoDm5g0Qt0fGvQ1NDvX5xVw62psbzn36kNZV
+// ihgGMHY2FDwstnw4uwk2Pjw71aDMJXbZo8wyeRbNA35fo9PeeYLbqGlFEYbZoRea
+// unXfPTl5NkA6BT+sJQIDAQAB
+// -----END PUBLIC KEY-----
