a09484025d2282197e2d6236fa94a4ef605115a6
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-03-06 19:56:26 +0530

    checkpoint: generate encrypted outpass

--- src/controllers/outpass.controller.ts
+++ src/controllers/outpass.controller.ts
@@ -1,37 +1,69 @@
-import { CustomError, throwError } from '../handlers/error.handler';
-import { Response } from '../types/ApiResponse';
+import { decryptData, encryptData } from '../handlers/encryption.handler';
+import { catchCustomError, throwError } from '../handlers/error.handler';
+import { asyncWrap } from '../middlewares/async';
+import { ApiResponse } from '../types/ApiResponse';
 import { getOutpass, getUserInfo } from './supabase.controller';
 
-export const getEncSignedOutpass = async (outpass_id: string) => {
-	let response = { data: {}, error: false, message: '' } as Response;
-	try {
-		let data = { outpass: {}, student: {}, warden: {} } as { outpass: any; student: any; warden: any };
-		let enc_string = '';
-		const outpass = await getOutpass(outpass_id);
-		if (!outpass.data) throwError(404, 'Outpass not found');
-		if (!outpass.data.student_id) throwError(404, 'Outpass not associated to student');
-		if (!outpass.data.signed_by) throwError(404, 'Outpass not yet signed');
+type OutpassData = {
+	o: any;
+	s: any;
+	w: any;
+};
 
-		data.outpass = { ...outpass.data };
+export const getEncSignedOutpass = asyncWrap(async (req, res, next) => {
+	let response = { status: 200, success: true, data: {}, message: '' } as ApiResponse;
+	try {
+		const outpass_id = req.params.id;
+		let data = { o: {}, s: {}, w: {} } as OutpassData;
+		const outpass = await getOutpass(outpass_id);
+		if (outpass.error) throwError(500, outpass.message);
+		if (!outpass.data) throwError(404, 'Outpass not found');
+		if (!outpass.data.student_id) throwError(400, 'Outpass not associated to student');
+		if (!outpass.data.signed_by) throwError(400, 'Outpass not yet signed');
+
+		// const { from, to, address, transport, reason, is_consent } = outpass.data;
+		// data.o = { f: from, t: to, a: address, tr: transport, r: reason, c: is_consent };
+		const { from, to } = outpass.data;
+		data.o = { f: from, t: to };
+
 		const student = await getUserInfo(outpass.data.student_id);
 		const warden = await getUserInfo(outpass.data.signed_by);
 
 		if (student.error) throwError(400, 'Error fetching student');
-		const { full_name: student_name, block, room, gender, course, regno } = student.data;
-		data.student = { name: student_name, block, room, gender, course, regno };
+		const { full_name: student_name, block, room, gender, regno } = student.data;
+		data.s = { n: student_name, b: block, r: room, g: gender, rn: regno };
 
-		if (warden.error) throwError(400, 'Error fetching warden signatory');
+		if (warden.error) throwError(400, 'Error fetching signatory');
 		const { full_name: warden_name } = warden.data;
-		data.warden = { name: warden_name };
+		data.w = warden_name;
 
-		response.data = { ...data };
+		const enc_string = encryptData(JSON.stringify(data));
+		if (!enc_string) throwError(500, 'Error encrypting data');
+
+		response.data = enc_string;
+		res.status(200).json(response);
 	} catch (error) {
-		console.log(error);
-		response.error = true;
-		if (error instanceof CustomError) {
-			response.message = error.message;
-		} else response.message = 'Error generating encrypted outpass';
-	} finally {
-		return response;
+		catchCustomError(error, next);
 	}
-};
+});
+
+export const getDecSignedOutpass = asyncWrap(async (req, res, next) => {
+	let response = { status: 200, success: true, data: {}, message: '' } as ApiResponse;
+	try {
+		if (!req.body) throwError(400, 'No body provided');
+		if (!req.body.data) throwError(400, 'No data provided');
+		const enc_data = req.body.data;
+
+		const dec_string = decryptData(JSON.stringify(enc_data));
+		if (!dec_string) throwError(500, 'Error decrypting data. Invalid key or data.');
+		try {
+			response.data = JSON.parse(dec_string || '{}');
+		} catch (error) {
+			console.log(error);
+			response.data = dec_string;
+		}
+		res.status(200).json(response);
+	} catch (error) {
+		catchCustomError(error, next);
+	}
+});
--- src/server.ts
+++ src/server.ts
@@ -2,40 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
-import { catchCustomError, ErrorHandler, throwError } from './handlers/error.handler';
-import { asyncWrap } from './middlewares/async';
-import { getEncSignedOutpass } from './controllers/outpass.controller';
-import { ApiResponse } from './types/ApiResponse';
+import bodyParser from 'body-parser';
+import { ErrorHandler, throwError } from './handlers/error.handler';
+import { getDecSignedOutpass, getEncSignedOutpass } from './controllers/outpass.controller';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.send(`Hello world!`);
-});
+app.get('/outpass/:id', getEncSignedOutpass);
+app.post('/outpass', getDecSignedOutpass);
 
-app.get(
-	'/outpass/:id',
-	asyncWrap(async (req, res, next) => {
-		let response = { status: 200, success: true, data: {}, message: '' } as ApiResponse;
-		try {
-			const outpass_id = req.params.id;
-			const { data, message, error } = await getEncSignedOutpass(outpass_id);
-			response.data = data;
-			if (error) throwError(500, message);
-			else res.status(200).json(response);
-		} catch (error) {
-			catchCustomError(error, next);
-		}
-	})
-);
-
-app.use(ErrorHandler);
-
-app.all('*', (_req, _res) => {
+app.all('*', () => {
 	throwError(500, 'Erroneous request');
 });
+
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server started at port: ${PORT}`);
